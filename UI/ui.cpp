ba810dcadc968db009edf3ca5518496e02aa72f5
Patrick Higgins <quickpanic@me.com>
2018-03-14 23:40:58 -0700

    change present to an enum

--- UI/ui.cpp
+++ UI/ui.cpp
@@ -11,7 +11,7 @@ UI::UI() {
 	refresh();
 }
 
-int UI::present_card(card* c) {
+performance UI::present_card(card* c) {
 	const char* card_front =  c->front().c_str();
 	int text_start_line = LINES / 3;
 	int text_start_col = COLS / 2 - c->front().length();
@@ -41,31 +41,31 @@ int UI::present_card(card* c) {
 	refresh();
 
 	waiting = true;
-	int performance = -1;
+	performance perf = unf;
 	while (waiting) {
 		int input = getch();
 		if (input == '1') {
-			performance = 0;
+			perf = bad;
 			waiting = false;
 		}
 
 		else if (input == '2') {
-			performance = 1;
+			perf = hard;
 			waiting = false;
 		}
 
 		else if (input == '3') {
-			performance = 2;
+			perf = easy;
 			waiting = false;
 		}
 
 		else if (input == '4') {
-			performance = 3;
+			perf = good;
 			waiting = false;
 		}
 	}
 
-	return performance;
+	return perf;
 }
 
 int UI::present_menu() {
--- UI/ui.hpp
+++ UI/ui.hpp
@@ -1,10 +1,11 @@
+#include "../Service/studyService.h"
 #include "../Modal/card.h"
 #include<ncurses.h>
 class UI {
 public:
 	UI();
 	int present_menu();
-	int present_card(card* c);
+	performance present_card(card* c);
 	~UI();
 private:
 	void draw_skeleton();
